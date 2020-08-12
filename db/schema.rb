f9e72faff537860c6fe462a5046c3f379b6fbd6c
Amonsta <amana.ammi@gmail.com>
2020-08-12 13:25:47 -0600

    migrated user & client tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_08_11_194730) do
+
+  create_table "clients", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.text "design_description"
+    t.string "location"
+    t.integer "price"
+    t.date "appt_date"
+    t.time "appt_time"
+    t.integer "phone_number"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_clients_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
