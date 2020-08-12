c655bd704e291015a7811ab57058baa6f7b89afe
Amonsta <amana.ammi@gmail.com>
2020-08-12 11:22:20 -0600

    user signup post and login form

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,8 +1,28 @@
 class UserController < ApplicationController
 
+    #Renders sign up form
     get '/users/signup' do 
         erb :'/users/signup'
     end 
+   
+    #creates the user, logs them in/starts session,
+    #redirect to user homepage
+    post '/users/signup' do 
+        @user = User.create(
+            name: params[:name],
+            email: params[:email],
+            password: params[:password]
+        )
+        session[:user_id]
+        redirect "/users/#{@user.id}"
+    end
+
+    #Renders login form
+    get '/users/login' do 
+       erb :'users/login'
+    end 
+
+    #Logs user in/session, Authenticate user
 
 
 end
